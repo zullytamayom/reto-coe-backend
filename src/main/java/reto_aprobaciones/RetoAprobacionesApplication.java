3478ed6c5cf3bc24b0bb51447c911e385f3458c6
zullytamayom <zullytamayom@gmail.com>
2026-07-11 17:57:36 -0500

    chore(setup): estructura inicial del backend desde spring initializr

---
+++ src/main/java/reto_aprobaciones/RetoAprobacionesApplication.java
@@ -0,0 +1,13 @@
+package reto_aprobaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAprobacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoAprobacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reto_aprobaciones/RetoAprobacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package reto_aprobaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoAprobacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
