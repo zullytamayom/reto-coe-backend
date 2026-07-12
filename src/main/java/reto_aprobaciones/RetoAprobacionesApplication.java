431f0e2b1785734091ae5f27a1b5af276eca3d64
zullytamayom <zullytamayom@gmail.com>
2026-07-12 09:39:25 -0500

    feat(servicio): implementar OutboxProcessor asincrono programado para AWS SNS

--- src/main/java/reto_aprobaciones/RetoAprobacionesApplication.java
+++ src/main/java/reto_aprobaciones/RetoAprobacionesApplication.java
@@ -4,36 +4,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.ListTopicsResponse;
 
 @SpringBootApplication
+@EnableScheduling
 public class RetoAprobacionesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetoAprobacionesApplication.class, args);
 	}
-    // Bean de prueba que se ejecuta automáticamente al iniciar la app
-    @Bean
-    public CommandLineRunner pruebaConexionAws(SnsClient snsClient) {
-        return args -> {
-            System.out.println("==================================================");
-            System.out.println("🚀 CASO DE PRUEBA: Validando conexión con AWS LocalStack...");
-            try {
-                // Intenta listar los temas creados por nuestro script init-aws.sh
-                ListTopicsResponse response = snsClient.listTopics();
-
-                System.out.println("✅ CONEXIÓN EXITOSA: ¡Spring Boot se conectó a LocalStack!");
-                System.out.println("📋 Temas de SNS encontrados en tu Mac:");
-                response.topics().forEach(topic ->
-                        System.out.println("   - ARN del Tema: " + topic.topicArn())
-                );
-            } catch (Exception e) {
-                System.out.println("❌ ERROR DE CONEXIÓN: No se pudo hablar con LocalStack.");
-                System.out.println("💡 Detalle del fallo: " + e.getMessage());
-            }
-            System.out.println("==================================================");
-        };
-    }
-
 }
